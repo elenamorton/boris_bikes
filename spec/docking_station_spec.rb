7c65d4931d073baf285705b48ebecadf5120bd42
Elena Morton <elmo@Elenas-MacBook-Pro.local>
2017-05-09 15:13:22 +0100

    step 10 - working? test passing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,4 +6,8 @@ describe DockingStation do
   #  expect(release_bike()).to eq false
   #end
   it { expect(subject).to respond_to :release_bike }
+
+  it "docking station to release a bike" do
+    expect(subject.release_bike).to be_working
+  end
 end
