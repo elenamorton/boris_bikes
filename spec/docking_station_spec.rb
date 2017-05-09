c80e3a46d2b687c3e1c1f568607c04aa650c406c
Elena Morton <elmo@Elenas-MacBook-Pro.local>
2017-05-09 16:28:59 +0100

    step 11 - use attr_reader for bike, and passed test.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,8 +6,24 @@ describe DockingStation do
   #  expect(release_bike()).to eq false
   #end
   it { expect(subject).to respond_to :release_bike }
+end
 
-  it "docking station to release a bike" do
-    expect(subject.release_bike).to be_working
-  end
+describe DockingStation do
+ it "docking station to release bike" do
+   expect(subject.release_bike).to be_working
+ end
+ it { is_expected.to respond_to(:dock_bike).with(1).argument}
+ it { is_expected.to respond_to(:bike)}
+
+ it "docks something" do
+   bike = Bike.new
+   expect(subject.dock_bike(bike)).to eq bike
+ end
+
+ it "returns bikes at dock station" do
+   bike = Bike.new
+   subject.dock_bike(bike)
+   expect(subject.bike).to eq bike
+ end
+ 
 end
